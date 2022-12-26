4d75969240279fb1b7ed965fae96dc361cd7ea00
drobinin575 <drobinin575@gmail.com>
2022-12-26 16:53:56 +0400

    added game-user bi-directional associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true, length: { maximum: 100 }
   validates :address, presence: true
   validates :datetime, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :games
+
   validates :name, presence: true, length: { maximum: 40 }
   validates :email, presence: true, uniqueness: true,
     format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/ }
