8f0447463ca5fd399474dcd41c5bcd86f6a0ed00
drobinin575 <drobinin575@gmail.com>
2023-01-06 13:08:05 +0400

    gem devise is installed and configured

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '3.1.2'
 
 gem 'bootsnap', require: false
 gem 'bootstrap', '~> 5.2.2'
+gem 'devise'
 gem 'importmap-rails'
 gem 'puma', '~> 5.0'
 gem 'rails', '~> 7.0.4'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
   has_many :games
 
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
   validates :name, presence: true, length: { maximum: 40 }
-  validates :email, presence: true, uniqueness: true,
-    format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/ }
+
+  before_validation :set_name
+
+  private
+
+  def set_name
+    self.name = "Gamer #{rand(777)}" if self.name.blank?
+  end
 end
