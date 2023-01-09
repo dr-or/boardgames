5cf73f81a8a01ad6015c68af6707176f910a62f6
drobinin575 <drobinin575@gmail.com>
2023-01-09 20:33:32 +0400

    added and designed devise views for new session and registration with bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user_can_edit?
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:name]
+    )
+  end
+
   private
 
   def current_user_can_edit?(game)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 40 }
-
-  before_validation :set_name, on: :create
-
-  private
-
-  def set_name
-    self.name = "Gamer #{rand(777)}" if self.name.blank?
-  end
 end
