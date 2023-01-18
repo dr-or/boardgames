10bd7384181ee8fcd21f0b3aa88b58a1632b3aaf
drobinin575 <drobinin575@gmail.com>
2023-01-18 14:47:22 +0400

    added associations with comments to game and user

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :address, presence: true
