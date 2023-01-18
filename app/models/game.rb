7a26db32b2aebf02cc9bdceb48bfb606b14631f5
drobinin575 <drobinin575@gmail.com>
2023-01-18 17:42:19 +0400

    added associations with subscriptions to game and user

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :subscriptions
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :address, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :games
   has_many :comments, dependent: :destroy
+  has_many :subscriptions
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
