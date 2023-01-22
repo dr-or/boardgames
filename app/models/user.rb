61938c763cc224b39893fbdcc53ceb07804ce4d4
drobinin575 <drobinin575@gmail.com>
2023-01-22 17:13:40 +0400

    link subscriptions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :subscriptions
 
+  after_commit :link_subscriptions, on: :create
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 40 }
+
+  private
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email).update(user_id: self.id)
+  end
 end
