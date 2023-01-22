a575d270a2d2fe7ecb940e8a7c33a9864ced9e9b
drobinin575 <drobinin575@gmail.com>
2023-01-22 18:18:40 +0400

    refactor model subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,27 +2,22 @@ class Subscription < ApplicationRecord
   belongs_to :game
   belongs_to :user, optional: true
 
-  validates :user_name, presence: true, unless: -> { user.present? }
-  validates :user_email, presence: true,
-            format: { with: /\A[\w\-.]+@[\w\-.]+\z/ },
-            unless: -> { user.present? }
+  with_options if: -> { user.present? } do
+    validates :user, uniqueness: {scope: :game_id}
+  end
 
-  validates :user, uniqueness: {scope: :game_id}, if: -> { user.present? }
-  validates :user_email, uniqueness: {scope: :game_id}, unless: -> { user.present? }
+  with_options unless: -> { user.present? } do
+    validates :user_name, presence: true
+    validates :user_email, presence: true,
+              format: { with: /\A[\w\-.]+@[\w\-.]+\z/ }
+    validates :user_email, uniqueness: {scope: :game_id}
+  end
 
   def user_name
-    if user.present?
-      user.name
-    else
-      super
-    end
+    user&.name || super
   end
 
   def user_email
-    if user.present?
-      user.email
-    else
-      super
-    end
+    user&.email || super
   end
 end
