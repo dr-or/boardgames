1631cf8a931a35b0084440a31e270239b3f3b799
drobinin575 <drobinin575@gmail.com>
2023-01-23 14:21:45 +0400

    email validation for subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -9,9 +9,9 @@ class Subscription < ApplicationRecord
 
   with_options unless: -> { user.present? } do
     validates :user_name, presence: true
-    validates :user_email, presence: true,
-              format: { with: /\A[\w\-.]+@[\w\-.]+\z/ }
+    validates :user_email, presence: true, format: { with: /\A[\w\-.]+@[\w\-.]+\z/ }
     validates :user_email, uniqueness: {scope: :game_id}
+    validate :registered_email
   end
 
   def user_name
@@ -26,5 +26,9 @@ class Subscription < ApplicationRecord
 
   def organizer_cant_be_subscriber
     errors.add(:user, :forbidden) if user == game.user
+  end
+
+  def registered_email
+    errors.add(:user_email, :taken) if User.exists?(email: user_email&.downcase)
   end
 end
