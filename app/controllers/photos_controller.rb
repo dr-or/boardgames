ce8d978382e1eb4ce5697be7b8f1ab5701084f9c
drobinin575 <drobinin575@gmail.com>
2023-02-05 14:00:06 +0400

    photos controller, routes and locales

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,42 @@
+class PhotosController < ApplicationController
+  before_action :set_game, only: %i[create destroy]
+  before_action :set_photo, only: %i[destroy]
+
+  def create
+    @new_photo = @game.photos.build(photo_params)
+    @new_photo.user = current_user
+
+    if @new_photo.save
+      redirect_to game_path(@game), notice: I18n.t("controllers.photos.created")
+    else
+      flash.now[:alert] = I18n.t("controllers.error")
+      render "games/show"
+    end
+  end
+
+  def destroy
+    message = {notice: I18n.t("controllers.photos.destroyed")}
+    
+    if current_user_can_edit?(@photo)
+      @photo.destroy
+    else
+      message = {alert: I18n.t("controllers.error")}
+    end
+
+    redirect_to game_path(@game), message
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:game_id])
+  end
+
+  def set_photo
+    @photo = @game.photos.find(params[:id])
+  end
+
+  def photo_params
+    params.fetch(:photo, {}).permit(:photo)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :games do
     resources :comments, only: %i[create destroy]
     resources :subscriptions, only: %i[create destroy]
+    resources :photos, only: %i[create destroy]
   end
   resources :users, only: %i[edit update show]
 end
