4d7a02c1867e4a9bdf6897eaaa28bc6a4973d93b
drobinin575 <drobinin575@gmail.com>
2023-02-07 15:06:58 +0400

    add GameMailer to comments and subscriptions controllers

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,6 +7,8 @@ class SubscriptionsController < ApplicationController
     @new_subscription.user = current_user
 
     if @new_subscription.save
+      GameMailer.subscription(@game, @new_subscription).deliver_now
+
       redirect_to game_path(@game), notice: I18n.t("controllers.subscriptions.created")
     else
       flash.now[:alert] = I18n.t("controllers.error")
