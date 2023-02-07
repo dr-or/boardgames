97a23536b20f26bf7d1b1125040bef3a24589236
drobinin575 <drobinin575@gmail.com>
2023-02-07 13:02:32 +0400

    add game_mailer

---
+++ app/mailers/game_mailer.rb
@@ -0,0 +1,17 @@
+class GameMailer < ApplicationMailer
+
+  def comment(comment, game, email)
+    @comment = comment
+    @game = game
+
+    mail to: email, subject: "#{I18n.t("game_mailer.comment.subject")} #{game.title}"
+  end
+  
+  def subscription(game, subscription)
+    @game = game
+    @email = subscription.user_email
+    @name = subscription.user_name
+
+    mail to: game.user.email, subject: "#{I18n.t("game_mailer.subscription.subject")} #{game.title}"
+  end
+end
