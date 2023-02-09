6dd20f3e119b96a712add07418640aecef063880
drobinin575 <drobinin575@gmail.com>
2023-02-09 22:22:50 +0400

    game subscription dependency

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :photos, dependent: :destroy
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :destroy
   has_many :subscribers, through: :subscriptions, source: :user
 
   validates :title, presence: true, length: { maximum: 100 }
