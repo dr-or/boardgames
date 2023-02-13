0a3045f1a43b520257d7e9d27e42970f9753e3a0
drobinin575 <drobinin575@gmail.com>
2023-02-13 16:06:42 +0400

    implemented pin code in routes, controller & view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,8 @@
 class GamesController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
+  before_action :set_game, only: %i[show]
   before_action :set_current_user_game, only: %i[edit update destroy]
+  before_action :password_guard!, only: %i[show]
 
   def index
     @games = Game.all
@@ -24,7 +26,6 @@ class GamesController < ApplicationController
   end
 
   def show
-    @game = Game.find(params[:id])
     @new_comment = @game.comments.build(params[:comment])
     @new_subscription = @game.subscriptions.build(params[:subscription])
   end
@@ -45,10 +46,28 @@ class GamesController < ApplicationController
   private
 
   def game_params
-    params.require(:game).permit(:title, :description, :address, :datetime)
+    params.require(:game).permit(:title, :description, :address, :datetime, :pincode)
+  end
+
+  def set_game
+    @game = Game.find(params[:id])
   end
 
   def set_current_user_game
     @game = current_user.games.find(params[:id])
+  end
+
+  def password_guard!
+    return true if @game.pincode.blank?
+    return true if user_signed_in? && @game.user == current_user
+
+    if params[:pincode].present? && @game.pincode == params[:pincode]
+      cookies.permanent["game_#{@game.id}_pincode"] = params[:pincode]
+    end
+
+    unless @game.pincode == cookies.permanent["game_#{@game.id}_pincode"]
+      flash.now[:alert] = I18n.t("controllers.games.wrong_pincode") if params[:pincode].present?
+      render "password_form"
+    end
   end
 end
