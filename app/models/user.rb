560fc0dd52ba009c41a22f6caa09fde75ee46fca
drobinin575 <drobinin575@gmail.com>
2023-03-31 15:07:32 +0400

    add dependencies to user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :games
+  has_many :games, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :destroy
   has_many :photos, dependent: :destroy
 
   after_commit :link_subscriptions, on: :create
