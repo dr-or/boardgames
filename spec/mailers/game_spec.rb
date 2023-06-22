bfc1d13612ca88bd530b82c5cc8f34ac663df645
dr-or <drobinin575@gmail.com>
2023-06-22 12:01:31 +0300

    test the game mailer

---
+++ spec/mailers/game_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe GameMailer, type: :mailer do
+  describe '#subscription' do
+    let(:game) { create(:game) }
+    let(:subscription) { create(:subscription, :with_present_user, game: game) }
+    let(:mail) { GameMailer.subscription(subscription) }
+
+    it 'renders the subject' do
+      expect(mail.subject).to include(I18n.t('game_mailer.subscription.subject', title: game.title))
+    end
+
+    it 'renders the mailto' do
+      expect(mail.to).to eq([game.user.email])
+    end
+
+    it 'renders the mailfrom' do
+      expect(mail.from).to eq([Rails.application.credentials.dig(:mail, :username)])
+    end
+  end
+end
